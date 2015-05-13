1143c765ccfac26ef94f4f0d1bd36ddf6f80f631
kt <kt@a2ndeiMac.local>
2015-05-13 10:35:35 +0800

    Initial Commit

---
+++ XIS_ Architecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XIS_ Architecture
+//
+//  Created by kt on 15/5/13.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
